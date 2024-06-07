4a48e1037cb83a01963397a0de70bdb002d372a6
Thomas Reese <thomas18reese@gmail.com>
2024-06-07 16:28:26 -0500

    finsihing the custom text and About Section

--- app/page.js
+++ app/page.js
@@ -5,13 +5,22 @@ const Page = () => (
   <div className='bg-primary-black overflow-hidden'>
     <Navbar />
     <Hero />
+    <div className='relative'>
     <About />
+    <div className='gradient-03 z-0' />
     <Explore />
+    </div>
+    <div className='relative'>
     <GetStarted />
+    <div className='gradient-04 z-0' />
     <WhatsNew />
+    </div>
     <World />
+    <div className='relative'>
     <Insights />
+    <div className='gradient-04 z-0' />
     <Feedback />
+    </div>
     <Footer />
   </div>
 );
