7403dcef3f66406f8514b787932fe51e9edb8ee0
Thomas Reese <thomas18reese@gmail.com>
2024-06-14 09:14:14 -0500

    inisight card section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -11,16 +11,16 @@ const Insights = () => (
     <section className={`${styles.paddings} relative z-10`}>
         <motion.div
             variants={staggerContainer}
-            initial='hidden'
-            whileInView='show'
-            viewport={{ once: 'false', amount: 0.25 }}
-            className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col`}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}
+            className={`${styles.innerWidth} mx-auto flex flex-col`}
         >
-            <TypingText title='| Insights' textStyles='text-center' />
-            <TitleText title='Insight about metaverse' textStyles='text-center' />
+            <TypingText title="| Insight" textStyles="text-center" />
+            <TitleText title={<>Insight about metaverse</>} textStyles="text-center" />
             <div className="mt-[50px] flex flex-col gap-[30px]">
-                {insights.map((insight, index) => (
-                    <InsightCard key={`insight-${index}`} {...insight} index={index + 1} />
+                {insights.map((item, index) => (
+                    <InsightCard key={`insight-${index}`} {...item} index={index + 1} />
                 ))}
             </div>
         </motion.div>
