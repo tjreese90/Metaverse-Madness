2f48ae7f78b7c92b1475a3e9cde8874cf3b40f20
Thomas Reese <thomas18reese@gmail.com>
2024-06-14 09:19:51 -0500

    footer and feedback section finalized

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,60 +1,62 @@
 'use client';
 import { motion } from 'framer-motion';
-import { socials } from '../constants';
 
 import styles from '../styles';
-import { footerVariants } from '../utils/motion';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
 
-const Footer = () => (
-    <motion.footer
-        variants={footerVariants}
-        initial="hidden"
-        whileInView="show"
-        className={`${styles.xPaddings} py-8 relative`}
-    >
-        <div className="footer-gradient" />
-        <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-            <div className="flex items-center justify-between flex-wrap gap-5">
-                <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-                    Enter the Metaverse
-                </h4>
-                <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-                    <img
-                        src="/headset.svg"
-                        alt="headset"
-                        className="w-[24px] h-[24px] object-contain"
-                    />
-                    <span className="font-normal text-[16px] text-white">
-                        Enter Metaverse
-                    </span>
-                </button>
-            </div>
-
-            <div className="flex flex-col">
-                <div className="mb-[50px] h-[2px] bg-white opacity-10" />
-
-                <div className="flex items-center justify-between flex-wrap gap-4">
-                    <h4 className="font-extrabold text-[24px] text-white">
-                        METAVERUS
+const Feedback = () => (
+    <section className={`${styles.paddings}`}>
+        <motion.div
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.25 }}
+            className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-6`}
+        >
+            <motion.div
+                variants={fadeIn('right', 'tween', 0.2, 1)}
+                className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
+            >
+                <div className="feedback-gradient" />
+                <div>
+                    <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
+                        Samantha
                     </h4>
-                    <p className="font-normal text-[14px] text-white opacity-50">
-                        Copyright © 2021 - 2022 Metaversus. All rights reserved.
+                    <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
+                        Founder Metaverus
                     </p>
-
-                    <div className="flex gap-4">
-                        {socials.map((social) => (
-                            <img
-                                key={social.name}
-                                src={social.url}
-                                alt={social.name}
-                                className="w-[24px] h-[24px] object-contain cursor-pointer"
-                            />
-                        ))}
-                    </div>
                 </div>
-            </div>
-        </div>
-    </motion.footer>
+
+                <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
+                    “With the development of today's technology, metaverse is very
+                    useful for today's work, or can be called web 3.0. by using
+                    metaverse you can use it as anything”
+                </p>
+            </motion.div>
+
+            <motion.div
+                variants={fadeIn('left', 'tween', 0.2, 1)}
+                className="relative flex-1 flex justify-center items-center"
+            >
+                <img
+                    src="/planet-09.png"
+                    alt="planet-09"
+                    className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+                />
+
+                <motion.div
+                    variants={zoomIn(0.4, 1)}
+                    className="lg:block hidden absolute -left-[10%] top-[3%]"
+                >
+                    <img
+                        src="/stamp.png"
+                        alt="stamp"
+                        className="w-[155px] h-[155px] object-contain"
+                    />
+                </motion.div>
+            </motion.div>
+        </motion.div>
+    </section>
 );
 
-export default Footer;
+export default Feedback;
