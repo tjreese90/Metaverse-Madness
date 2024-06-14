b3653a1d4a8ba3b1c4e7afb708310a750e7d4c4b
Thomas Reese <thomas18reese@gmail.com>
2024-06-14 18:04:19 -0500

    final changes

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,18 +1,16 @@
 'use client';
 
+
 import { motion } from 'framer-motion';
-import styles from '../styles';
-import { slideIn, staggerContainer, textContainer, textVariant, textVariant2 } from '../utils/motion';
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
     <motion.p
         variants={textContainer}
-        className={`font-normal text-[14px]
-         text-secondary-white ${textStyles}`}>
+        className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+    >
         {Array.from(title).map((letter, index) => (
-            <motion.span
-                variants={textVariant2}
-                key={index}>
+            <motion.span variants={textVariant2} key={index}>
                 {letter === ' ' ? '\u00A0' : letter}
             </motion.span>
         ))}
@@ -22,8 +20,8 @@ export const TypingText = ({ title, textStyles }) => (
 export const TitleText = ({ title, textStyles }) => (
     <motion.h2
         variants={textVariant2}
-        initial='hidden'
-        whileInView='show'
+        initial="hidden"
+        whileInView="show"
         className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
     >
         {title}
